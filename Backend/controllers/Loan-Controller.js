2cff6e607708655e192fac8264e93e707cfe2664
sribhargav1345 <sribhargav1345@gmail.com>
2024-10-17 15:19:01 +0530

    debugging

--- Backend/controllers/Loan-Controller.js
+++ Backend/controllers/Loan-Controller.js
@@ -35,7 +35,7 @@ router.get("/loans", async (req, res) => {
     } 
     catch (error) {
         console.error('Error getting loans:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json(error);
     }
 });
 
